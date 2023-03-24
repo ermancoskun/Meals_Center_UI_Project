d8898658961c50f77023d39317e137e4faea5053
Mehmet Erden <merden2512@gmail.com>
2023-03-24 15:27:19 +0300

    localde yeni calısmalar

--- src/test/java/tests/US_007_018_033_040/US_018.java
+++ src/test/java/tests/US_007_018_033_040/US_018.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import pages.Admin_Dashboard;
 import pages.Merchant_Dashboard;
 import pages.User_Homepage;
+import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
@@ -26,20 +27,24 @@ public class US_018 {
         //1)bir browser aç.
         //2)"https://qa.mealscenter.com/" url uzerinden 'mealscenter' sitesine gider.
         //3)Cookies kabul edilir.
-        ReusableMethods.goTouserHomePage();
+       /* ReusableMethods.goTouserHomePage();
         //4)Sign in butonuna basar.
-        ud.homePageSignInButton.click();
+        ud.signInButton.click();
         WebElement emailbox= Driver.getDriver().findElement(By.xpath("//label[@for='username']"));
         actions.sendKeys(emailbox,"merden25@gmail.com")
                 .sendKeys(Keys.TAB)
                 .sendKeys("M123456789")
                 .click(Driver.getDriver().findElement(By.xpath("//span[text()='Sign in']"))).perform();
 
+        */
+
         //5)geccerli emailini email kutusuna yazar.
 
         //6)gecerli sifresini sifre kutusuna yazar.
         //7)Sign in butonuna basar.
-        ReusableMethods.wait(2);
+        //ReusableMethods.wait(2);
+        ReusableMethods.goTouserHomePage();
+        ReusableMethods.userLoginMethod(ConfigReader.getProperty("betulUserEmail"),ConfigReader.getProperty("betulUserPassword"));
         WebElement userName=Driver.getDriver().findElement(By.xpath("//li/div/a"));
         //8)basarili giris yapıldı yazisinin cıktıgını dogrular.
         //9)giris yapildıktan sonra kendi isminin sayfanın sag ust kosesinde yazdigini kontrol eder
